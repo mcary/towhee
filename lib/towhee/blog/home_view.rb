a8c4804c255438433b39ac8ffc6cebf956c472f1
Marcel M. Cary <marcel@oak.homeunix.org>
2018-04-07 18:30:52 -0700

    Use HTML writer in blog view(s)
    Apply the Towhee::HTML module's Writer API to generate the HTML produced
    by the Towhee::Blog's HomeView to assess usefulness from the perspective
    of a API's consumer.

--- lib/towhee/blog/home_view.rb
+++ lib/towhee/blog/home_view.rb
@@ -1,10 +1,12 @@
 require 'towhee'
+require 'towhee/html/writer'
 
 module Towhee::Blog
   class HomeView
     def initialize(site, posts: [])
       @site = site
       @posts = posts
+      @html = Towhee::HTML::Writer.new
     end
 
     def path
@@ -12,37 +14,28 @@ module Towhee::Blog
     end
 
     def render
-      <<-END
-        <html>
-          <h1>#{@site.name} Home<h1>
-
-          #{if @posts.any?
-              post_list
-            else
-              empty_message
-            end }
-        </html>
-      END
+      @html.html do
+        @html.h1 { @site.name + " Home" } +
+          if @posts.any?
+            post_list
+          else
+            empty_message
+          end
+      end.to_s
     end
 
     private
 
     def post_list
-      <<-END
-        <ul>
-          #{post_list_items}
-        </ul>
-      END
-    end
-
-    def post_list_items
-      @posts.map do |post|
-        "<li>#{post.title}</li>"
-      end.join
+      @html.ul do
+        @posts.map do |post|
+          @html.li { post.title }
+        end.inject(Towhee::HTML::Fragment.new(""), &:+)
+      end
     end
 
     def empty_message
-      "<p>No posts yet.</p>"
+      @html.p { "No posts yet." }
     end
   end
 end
--- spec/blog/home_view_spec.rb
+++ spec/blog/home_view_spec.rb
@@ -20,4 +20,10 @@ RSpec.describe Towhee::Blog::HomeView do
     view = described_class.new(site, posts: posts)
     expect(view.render).to match /Some Post/
   end
+
+  it "renders posts without escaping <li> tags" do
+    posts = [Towhee::Blog::Post.new(title: "Some Post")]
+    view = described_class.new(site, posts: posts)
+    expect(view.render).to match /<li>\s*Some Post/m
+  end
 end
