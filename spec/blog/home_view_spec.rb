448b9b427f97ddb256c65d8d2266c4b05769892b
Marcel M. Cary <marcel@oak.homeunix.org>
2018-04-11 19:23:16 -0700

    Organize home_view_spec by context

--- spec/blog/home_view_spec.rb
+++ spec/blog/home_view_spec.rb
@@ -5,30 +5,32 @@ require 'towhee/blog/post'
 RSpec.describe Towhee::Blog::HomeView do
   let(:site) { Towhee::Blog::Site.new(name: "Some Site") }
 
-  it "has root path" do
-    view = described_class.new(site)
-    expect(view.path).to eq "index.html"
+  context "with no posts" do
+    let(:view) { described_class.new(site) }
+
+    it "has root path" do
+      expect(view.path).to eq "index.html"
+    end
+
+    it "renders site name" do
+      expect(view.render).to match /Some Site Home/
+    end
+
+    it "renders empty message" do
+      expect(view.render).to match /No posts yet./
+    end
   end
 
-  it "renders site name" do
-    view = described_class.new(site)
-    expect(view.render).to match /Some Site Home/
-  end
+  context "with a post" do
+    let(:posts) { [Towhee::Blog::Post.new(title: "Some Post")] }
+    let(:view) { described_class.new(site, posts: posts) }
 
-  it "renders empty message" do
-    view = described_class.new(site)
-    expect(view.render).to match /No posts yet./
-  end
+    it "renders posts" do
+      expect(view.render).to match /Some Post/
+    end
 
-  it "renders posts" do
-    posts = [Towhee::Blog::Post.new(title: "Some Post")]
-    view = described_class.new(site, posts: posts)
-    expect(view.render).to match /Some Post/
-  end
-
-  it "renders posts without escaping <li> tags" do
-    posts = [Towhee::Blog::Post.new(title: "Some Post")]
-    view = described_class.new(site, posts: posts)
-    expect(view.render).to match /<li>\s*Some Post/m
+    it "renders posts without escaping <li> tags" do
+      expect(view.render).to match /<li>\s*Some Post/m
+    end
   end
 end
