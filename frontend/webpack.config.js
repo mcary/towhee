bb7cc4ad6818cf87a4eb0f905aa9f7d52b5fac0f
Marcel M. Cary <marcel@oak.homeunix.org>
2018-05-24 17:39:30 -0700

    Configure Scss sourceMaps for debugging

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,11 @@ module.exports = {
   module: {
     rules: [
       { test: /\.css/, use: ['style-loader', 'css-loader'] },
-      { test: /\.scss/, use: ['style-loader', 'css-loader', 'sass-loader'] }
+      { test: /\.scss/, use: [
+        'style-loader',
+        { loader: 'css-loader', options: { sourceMaps: true } },
+        { loader: 'sass-loader', options: { sourceMaps: true } }
+      ]},
     ]
   }
 }
