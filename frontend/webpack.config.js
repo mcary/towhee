7923ae3f0a4cd6bb118ba1f2baf028f32dbca6bc
Marcel M. Cary <marcel@oak.homeunix.org>
2018-05-26 08:21:34 -0700

    Use mini-css-extract-plugin to extract CSS from JS for production
    In development we need to embed CSS in a JS file to support live reload,
    the super-slick feature that causes HTML, CSS, and JS to be replaced in
    a running browser without a page refresh whenever a file on disk
    changes.
    
    On the other hand, in production, we want to load CSS directly from a
    CSS file.  So we need a way to build a separate file for production
    only.
    
    Use mini-css-extract-plugin to extact the CSS that would have gone into
    the JS file into its own CSS file.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,13 +1,24 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// Set this env var by prefixing the command with:
+//   NODE_ENV=production
+const style_loader = process.env.NODE_ENV !== 'production' ?
+  'style-loader' :
+  MiniCssExtractPlugin.loader;
+
 module.exports = {
   devServer: {
     contentBase: './dist',
     watchContentBase: true
   },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
   module: {
     rules: [
-      { test: /\.css/, use: ['style-loader', 'css-loader'] },
+      { test: /\.css/, use: [style_loader, 'css-loader'] },
       { test: /\.scss/, use: [
-        'style-loader',
+        style_loader,
         { loader: 'css-loader', options: { sourceMaps: true } },
         { loader: 'sass-loader', options: { sourceMaps: true } }
       ]},
