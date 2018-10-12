0f5d1378e3cebb2ac375441f7f8a73abe81cf48e
Marcel M. Cary <marcel@oak.homeunix.org>
2018-10-11 17:12:20 -0700

    Add a MemoryAdapter that quacks like an ActiveRecordAdapter

---
+++ lib/towhee/multi_table_inheritance/memory_adapter.rb
@@ -0,0 +1,66 @@
+require 'towhee/multi_table_inheritance'
+
+module Towhee::MultiTableInheritance
+  class MemoryAdapter
+    def initialize
+      # table => array of row hashes
+      @data = Hash.new {|hsh, key| hsh[key] = [] }
+      @last_id = 0
+    end
+
+    def select_all_from(table, key, vals)
+      key = sanitize_key(key)
+      rows = @data[table].select do |row|
+        vals.include? row[key]
+      end
+      clone rows
+    end
+
+    def select_from(table, key, val)
+      select_all_from(table, key, [val]).first
+    end
+
+    def insert(table, row)
+      row = normalize_row(row)
+      internal_row = clone(row)
+      id = next_id
+      internal_row["id"] = id
+      @data[table].push internal_row
+      id
+    end
+
+    def update(table, entity_id, row)
+      row = normalize_row(row)
+      internal_row = @data[table].find do |row|
+        entity_id == row["entity_id"]
+      end
+      internal_row.merge!(clone(row))
+      nil
+    end
+
+    private
+
+    def next_id
+      @last_id += 1
+    end
+
+    def normalize_row(row)
+      Hash[row.map {|k,v| [sanitize_key(k), sanitize_val(v)] }]
+    end
+
+    def clone(data)
+      Marshal.load(Marshal.dump(data))
+    end
+
+    def sanitize_val(val)
+      raise unless val.is_a? String
+      val
+    end
+
+    def sanitize_key(key)
+      raise unless key.is_a?(String) || key.is_a?(Symbol)
+      key.to_s
+    end
+  end
+end
+
---
+++ spec/multi_table_inheritance/memory_adapter_spec.rb
@@ -0,0 +1,122 @@
+require 'towhee/multi_table_inheritance/memory_adapter'
+
+RSpec.describe Towhee::MultiTableInheritance::MemoryAdapter do
+  it "round-trips a row" do
+    id = subject.insert("foos", "attr1" => "val1")
+    row = subject.select_from("foos", "attr1", "val1")
+    expect(row).to include("attr1" => "val1")
+    expect(id).to eq 1
+    expect(row).to include("id" => 1)
+  end
+
+  it "clones data" do
+    val = "val1"
+    id = subject.insert("foos", "attr1" => val)
+    row = subject.select_from("foos", "attr1", "val1")
+    expect(row["attr1"]).to eq val
+    expect(row["attr1"]).not_to equal val # Compare object idenity
+  end
+
+  it "returns a symbol row by symbols" do
+    id = subject.insert("foos", attr1: "val1")
+    row = subject.select_from("foos", :attr1, "val1")
+    expect(row).to include("attr1" => "val1")
+  end
+
+  it "returns a string row by symbols" do
+    id = subject.insert("foos", "attr1" => "val1")
+    row = subject.select_from("foos", :attr1, "val1")
+    expect(row).to include("attr1" => "val1")
+  end
+
+  describe "#insert" do
+    it "returns auto-incremented IDs" do
+      id1 = subject.insert("foos", "attr1" => "val1")
+      id2 = subject.insert("foos", "attr1" => "val2")
+      row = subject.select_from("foos", "attr1", "val1")
+      expect(id1).to eq 1
+      expect(id2).to eq 2
+      expect(row).to include("id" => 1)
+    end
+  end
+
+  describe "#select_from" do
+    it "returns nil if none match" do
+      subject.insert("foos", "attr1" => "val1")
+      row = subject.select_from("foos", "attr1", "val2")
+      expect(row).to eq nil
+    end
+
+    it "clones" do
+      id = subject.insert("foos", "attr1" => "val1")
+      row = subject.select_from("foos", "id", id)
+      row["attr1"] = "val2" # mutate
+      row = subject.select_from("foos", "id", id)
+      expect(row["attr1"]).to eq "val1"
+    end
+  end
+
+  describe "#select_all_from" do
+    it "finds all matches" do
+      subject.insert("foos", "attr1" => "val1")
+      subject.insert("foos", "attr1" => "val1")
+      rows = subject.select_all_from("foos", "attr1", ["val1"])
+      expect(rows.size).to eq 2
+      rows.sort_by! {|row| row["id"] }
+      expect(rows.first).to include("attr1" => "val1")
+      expect(rows.last).to include("attr1" => "val1")
+      expect(rows.first).to include("id" => 1)
+      expect(rows.last).to include("id" => 2)
+    end
+
+    it "finds only matches" do
+      subject.insert("foos", "attr1" => "val1")
+      subject.insert("foos", "attr1" => "val2")
+      rows = subject.select_all_from("foos", "attr1", ["val1"])
+      expect(rows.size).to eq 1
+      expect(rows.first).to include("attr1" => "val1")
+    end
+
+    it "finds none if no matches" do
+      subject.insert("foos", "attr1" => "val1")
+      rows = subject.select_all_from("foos", "attr1", ["val2"])
+      expect(rows.size).to eq 0
+    end
+
+    it "clones" do
+      id = subject.insert("foos", "attr1" => "val1")
+      rows = subject.select_all_from("foos", "id", [id])
+      rows.first["attr1"] = "val2" # mutate
+      rows = subject.select_all_from("foos", "id", [id])
+      expect(rows.first["attr1"]).to eq "val1"
+    end
+  end
+
+  describe "#update" do
+    it "mutates a row" do
+      entity_id = "3"
+      id = subject.insert("foos", "entity_id" => entity_id, "attr1" => "val1")
+      subject.update("foos", entity_id, "attr1" => "val2")
+      row = subject.select_from("foos", "id", id)
+      expect(row).to include("attr1" => "val2")
+    end
+
+    it "mutates only matches" do
+      subject.insert("foos", "entity_id" => "3", "a1" => "v1")
+      subject.insert("foos", "entity_id" => "4", "a1" => "v1")
+      subject.update("foos", "3", "a1" => "v2")
+      expect(subject.select_from("foos", "entity_id", "3")["a1"]).to eq "v2"
+      expect(subject.select_from("foos", "entity_id", "4")["a1"]).to eq "v1"
+    end
+  end
+
+  describe "#insert" do
+    it "clones" do
+      row = { "attr1" => "val1" }
+      id = subject.insert("foos", row)
+      row["attr1"] = "val2" # mutate
+      row = subject.select_from("foos", "id", id)
+      expect(row["attr1"]).to eq "val1"
+    end
+  end
+end
