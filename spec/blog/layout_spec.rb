99b7031a11cbb717c9ba07d65913ac77bd52d884
Marcel M. Cary <marcel@oak.homeunix.org>
2019-02-14 17:48:42 -0800

    Test Blog::Layout structure using REXML to parse the HTML
    So far, I've gotten by with just Regexps do test view output.  This
    feels clumsy but doesn't require any external dependencies.  But with
    the Layout, "contains" relationships become more important.
    
    Add support for testing containment of text within elements.  For any
    text that is not a direct child of that element, this is hard to do with
    Regexps, so use REXML instead.
    
    Use REXML instead of nokogiri, which may be better suited to the job, to
    avoid an external dependency of this library.  If this becomes a common
    need, maybe we can give the user a choice of driver someday.

--- spec/blog/layout_spec.rb
+++ spec/blog/layout_spec.rb
@@ -1,6 +1,8 @@
 require 'towhee/blog/layout'
 require 'towhee/html/writer'
 require 'towhee/blog/site'
+require 'rexml/document'
+require 'rexml/xpath'
 
 RSpec.describe Towhee::Blog::Layout do
   let(:view) do
@@ -34,5 +36,53 @@ RSpec.describe Towhee::Blog::Layout do
 
   it "renders a header with site name" do
     expect(view.render).to match %r{<h1 class="display-1">\s*A Site\s*</h1>}
+  end
+
+  describe "structure" do
+    it "renders heading inside full width column" do
+      find(view.render, ".container .col-12", contains: "A Site")
+    end
+
+    it "renders main content inside main column" do
+      find(view.render, ".container .col-8", contains: "Main content")
+    end
+
+    it "renders sidebar inside side column" do
+      find(view.render, ".container .col-4", contains: "Module 1")
+    end
+  end
+
+  def find(markup, selector, contains: nil)
+    matches = css(markup, selector, contains: contains)
+    custom_msg = "Expected to find selector '#{selector}' in:\n#{markup}"
+    expect(matches.size).to eq(1), custom_msg
+    matches.first
+  end
+
+  # Only a subset of CSS is supported: .class1 .class2 ...
+  def css(markup, selector, contains: nil)
+    path = compile_css_to_xpath(selector, contains: contains)
+    xpath(markup, path)
+  end
+
+  # Not sure REXML is the way to go here... but it avoids a dependency on
+  # a C library (nokogiri).
+  def xpath(markup, path)
+    node = REXML::Document.new(markup).root
+    REXML::XPath.match(node, path)
+  end
+
+  def compile_css_to_xpath(selector, contains: nil)
+    exprs = selector.split
+    path = exprs.inject("") do |path, expr|
+      if expr.start_with?(".")
+        klass = expr.sub(".", "")
+        path += "//*[@class='#{klass}']"
+      else
+        raise "Unsupported CSS expression: #{expr} in: #{selector}"
+      end
+    end
+    path += "[contains(., '#{contains}')]" if contains
+    path
   end
 end
